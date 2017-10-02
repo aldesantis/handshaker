6fdaf7ac05c2b49bb86c22ce4bd2ae8cb0db3ffa
Seva Rybakov <jumpincode@gmail.com>
2017-10-02 17:02:39 +0300

    Add step contributions

--- lib/handshaker/step/base.rb
+++ lib/handshaker/step/base.rb
@@ -4,9 +4,19 @@ module Handshaker
   module Step
     class Base
       attr_reader :party
+      attr_reader :contribution
 
       def initialize(party:)
         @party = party
+        @contibution = nil
+      end
+
+      def contribute(contribution)
+        @contribution = contribution
+      end
+
+      def contributed?
+        !contribution.nil?
       end
     end
   end
--- spec/handshaker/step/base_spec.rb
+++ spec/handshaker/step/base_spec.rb
@@ -3,13 +3,31 @@
 require 'spec_helper'
 
 RSpec.describe Handshaker::Step::Base do
-  describe 'constructor' do
-    subject { described_class.new(party: party) }
+  subject { described_class.new(party: party) }
 
-    let(:party) { 'buyer' }
+  let(:party) { 'buyer' }
 
+  describe 'initializer' do
     it 'sets party' do
       expect(subject.party).to eq(party)
+    end
+
+    it 'sets contribution to nil' do
+      expect(subject.contribution).to be_nil
+    end
+  end
+
+  describe '#contribute' do
+    let(:contribution) { 100 }
+
+    before { subject.contribute(contribution) }
+
+    it 'sets contribution' do
+      expect(subject.contribution).to eq(contribution)
+    end
+
+    it 'makes step contributed' do
+      expect(subject).to be_contributed
     end
   end
 end
